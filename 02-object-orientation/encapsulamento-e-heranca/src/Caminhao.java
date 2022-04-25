47cf20d601dacf4cbf89262e58bbddd56ce85f34
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-25 09:50:43 -0300

    add inheritance first example

---
+++ 02-object-orientation/encapsulamento-e-heranca/src/Caminhao.java
@@ -0,0 +1,8 @@
+public class Caminhao extends Veiculo{
+  private String placa;
+  private String motorista;
+
+  public Caminhao(String placa, String motorista, String cor) {
+    super(placa, motorista, cor);
+  }
+}
---
+++ 02-object-orientation/encapsulamento-e-heranca/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    Caminhao volks = new Caminhao("abc-123", "Raphael", "Prata");
+
+    System.out.println("Placa do caminhão: " + volks.getPlaca());
+  }
+}
