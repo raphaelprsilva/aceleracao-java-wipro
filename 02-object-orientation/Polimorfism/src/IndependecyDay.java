04b7d66e72a0bfa8edf30fface24e531cf3c68f7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-26 20:55:42 -0300

    Add IndependecyDay Class

---
+++ 02-object-orientation/Polimorfism/src/IndependecyDay.java
@@ -0,0 +1,10 @@
+public class IndependecyDay extends Movie {
+  public IndependecyDay() {
+    super("Independecy Day");
+  }
+
+  @Override
+  public String plot() {
+    return "Aliens attempt to take over the planet";
+  }
+}
