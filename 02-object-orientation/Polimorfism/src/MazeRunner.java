7074b192233823378a01f500b6bc67b975f9590e
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-26 20:56:05 -0300

    Add MazeRunner Class

---
+++ 02-object-orientation/Polimorfism/src/MazeRunner.java
@@ -0,0 +1,10 @@
+public class MazeRunner extends Movie {
+  public MazeRunner() {
+    super("Maze Runner");
+  }
+
+  @Override
+  public String plot() {
+    return "Kids trying to scape a maze.";
+  }
+}
