c2fc68ea8fe1e4667258fc828fed6bbb1afa3eff
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-27 13:48:50 -0300

    implements Ferrari clas

---
+++ 02-object-orientation/polymorphism-challange/src/Ferrari.java
@@ -0,0 +1,21 @@
+public class Ferrari extends Car {
+
+  public Ferrari(int cylinders, String name) {
+    super(cylinders, name);
+  }
+
+  @Override
+  public void startEngine() {
+    super.startEngine();
+  }
+
+  @Override
+  public void accelerate() {
+    super.accelerate();
+  }
+
+  @Override
+  public void breakCar() {
+    super.breakCar();
+  }
+}
