04f606d2ed44a7e73065bd52468599934fc09ed7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-26 21:05:41 -0300

    Add randomMovie method

--- 02-object-orientation/Polimorfism/src/Main.java
+++ 02-object-orientation/Polimorfism/src/Main.java
@@ -2,4 +2,23 @@ public class Main {
   public static void main(String[] args) {
 
   }
+
+  public static Movie randomMovie() {
+    int randomNumber = (int) (Math.random() * 5) + 1;
+    System.out.println("Random number generated was: " + randomNumber);
+
+    switch (randomNumber) {
+      case 1:
+        return new Jaws();
+      case 2:
+        return new IndependecyDay();
+      case 3:
+        return new MazeRunner();
+      case 4:
+        return new StarWars();
+      case 5:
+        return new ForgetableMovie();
+    }
+    return null;
+  }
 }
