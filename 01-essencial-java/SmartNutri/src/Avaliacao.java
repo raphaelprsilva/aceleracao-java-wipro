acba531af35eb092f4f88d931a6a03d86f3173c4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-28 18:11:49 -0300

    remove files from older directory

--- 01-essencial-java/SmartNutri/src/Avaliacao.java
+++
@@ -1,6 +0,0 @@
-public class Avaliacao {
-  public void calcularIMC(double peso, double altura) {
-    double IMC = peso / Math.pow(altura, 2);
-    System.out.println("O seu IMC é " + IMC);
-  }
-}
