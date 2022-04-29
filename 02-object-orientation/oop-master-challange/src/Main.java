f5bfd740cd02dc005aa73cbe20d5e36a8619f175
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-29 08:18:16 -0300

    implements Main class

--- 02-object-orientation/oop-master-challange/src/Main.java
+++ 02-object-orientation/oop-master-challange/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    Hamburguer hamburguer = new Hamburguer("Pão Especial", "Hamburgurer de Porco", 15.00);
+    Hamburguer hamburguer = new Hamburguer("My Burguer", "Pão Especial", "Hamburgurer de Porco", 15.00);
+    double price = hamburguer.itemizeHamburguer();
+    hamburguer.addHamburguerAddition1("tomato", 1.2);
+    hamburguer.addHamburguerAddition2("lettuce", 2.3);
+    System.out.println("Preço total: " + hamburguer.itemizeHamburguer());
 
+    HealthyBurguer healthyBurguer = new HealthyBurguer("Bacon", 5.0);
+    healthyBurguer.addHamburguerAddition1("Egg", 2.5);
+    System.out.println("Preço total do Healthy Burger: " + healthyBurguer.itemizeHamburguer());
+
+    DeluxeHamburguer deluxeHamburguer = new DeluxeHamburguer();
+    deluxeHamburguer.itemizeHamburguer();
+    deluxeHamburguer.addHamburguerAddition1("Item 1", 3.5);
   }
 }
