3b7a42d4d626819f3178db7caf84f0fdab201fed
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-04-29 15:37:05 -0300

    add challange

---
+++ 03-arrays-inbuilt-lists-autoboxing/01-01-arrays-challange/README.md
@@ -0,0 +1,15 @@
+# Desafio
+
+Crie um programa usando arrays. Este deve ordenar uma lista de números inteiros em ordem decrescente.
+
+> Decrescente significa ordenar do maior para o menor.
+
+Em outras palavras, o usuário vai entrar com os valores via console.
+
+Por exemplo: o usuário entra com os números 106, 26, 81, 5, 15 e o seu programa deve ser capaz armazená-los em um array.
+
+Implemente 3 métodos para auxiliá-lo:
+
+- `getIntegers`: retorna um array dos números inteiros que o usuário entrar
+- `printArray`: printa na tela o conteúdo do array, ou seja, não retorna nada
+- `sortIntegers`: retorna um novo array com os números ordenados
---
+++ 03-arrays-inbuilt-lists-autoboxing/01-01-arrays-challange/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    int[] myArray = getIntegers(6);
+    int[] sortedArray = sortIntegers(myArray);
+    printArray(sortedArray);
+  }
+
+  public static int[] getIntegers(int arraySize) {
+    int[] array = new int[arraySize];
+    System.out.println("Digite " + arraySize + " números para serem inseridos no array: ");
+
+    for (int index = 0; index < array.length; index += 1) {
+      array[index] = scanner.nextInt();
+    }
+
+    return array;
+  }
+
+  public static void printArray(int[] array) {
+    System.out.println("array: " + Arrays.toString(array));
+  }
+
+  public static int[] sortIntegers(int[] array) {
+    int[] sortedArray = new int[array.length];
+
+    // Faz uma cópia do array original
+    for (int i = 0; i < array.length; i += 1) {
+      sortedArray[i] = array[i];
+    }
+
+    for (int i = 0; i < sortedArray.length; i += 1) {
+      for (int j = 0; j < sortedArray.length - i - 1; j += 1) {
+        if (sortedArray[j] < sortedArray[j + 1]) {
+          int temp = sortedArray[j];
+          sortedArray[j] = sortedArray[j + 1];
+          sortedArray[j + 1] = temp;
+        }
+      }
+    }
+
+    return sortedArray;
+  }
+}
