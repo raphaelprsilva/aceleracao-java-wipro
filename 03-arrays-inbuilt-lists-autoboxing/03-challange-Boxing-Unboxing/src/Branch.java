debd8409edddd55c53ab78d25dadf3c1acbc2fd0
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-04 14:26:22 -0300

    add Branch class constructor

--- 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Branch.java
+++ 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Branch.java
@@ -1,3 +1,11 @@
-public class Branch {
+import java.util.ArrayList;
 
+public class Branch {
+  private String name;
+  private ArrayList<Customer> customers;
+
+  public Branch(String name) {
+    this.name = name;
+    this.customers = new ArrayList<Customer>();
+  }
 }
