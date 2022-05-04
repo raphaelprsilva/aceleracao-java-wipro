e7a907bc6732371de9bba9a0a81b40a7e82260dc
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-04 14:20:30 -0300

    add attributes and constructor

--- 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Customer.java
+++ 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Customer.java
@@ -1,3 +1,12 @@
-public class Customer {
+import java.util.ArrayList;
 
+public class Customer {
+  private String name;
+  private ArrayList<Double> transactions;
+
+  public Customer(String name, double initialTransaction) {
+    this.name = name;
+    this.transactions = new ArrayList<Double>();
+    this.transactions.add(initialTransaction);
+  }
 }
