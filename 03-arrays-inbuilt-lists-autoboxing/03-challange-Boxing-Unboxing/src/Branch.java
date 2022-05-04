86fadf25f07fdf0c444eba0a5ba0cdbf6c7837b9
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-04 16:23:51 -0300

    add newCustomer and addCustomerTransaction methods

--- 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Branch.java
+++ 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Branch.java
@@ -17,6 +17,30 @@ public class Branch {
     return this.customers;
   }
 
+  public boolean newCustomer(String clientName, double initialTransaction) {
+    if (findCustomer(clientName) >= 0) {
+      System.out.println("Não foi possível adicionar o cliente. Já existe um cliente com o nome " + clientName);
+      return false;
+    }
+
+    Customer customer = new Customer(clientName, initialTransaction);
+    this.customers.add(customer);
+    System.out.println("Cliente " + clientName + " adicionado com sucesso. Depósito inicial: " + initialTransaction);
+    return true;
+  }
+
+  public boolean addCustomerTransaction(String clientName, double transaction) {
+    // 1. Verificar se o cliente já existe
+    // 2. Se o cliente já existir, somar o valor de transaction ao depósito inicial já feito.
+    // 4. Caso contrário, recusar a transação, ou seja, retornar falso.
+    if (findCustomer(clientName) >= 0) {
+      Customer customer = this.customers.get(findCustomer(clientName));
+      customer.addTransaction(transaction);
+      return true;
+    }
+    return false;
+  }
+
   public int findCustomer(String clientName) {
     for (int index = 0; index < this.customers.size(); index += 1) {
       Customer customer = this.customers.get(index);
