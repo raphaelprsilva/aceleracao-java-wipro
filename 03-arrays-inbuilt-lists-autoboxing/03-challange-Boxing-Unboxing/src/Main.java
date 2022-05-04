d5e37b71d20544cffd34e080683151b250108782
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-04 19:31:20 -0300

    implements main method to test

--- 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Main.java
+++ 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
   public static void main(String[] args) {
+    Bank bank = new Bank("Banco Nacional");
 
+    bank.addBranch("Brasília");
+
+    bank.addCustomer("Brasília", "Tim", 50.05);
+    bank.addCustomer("Brasília", "Mike", 175.34);
+    bank.addCustomer("Brasília", "Percy", 220.12);
+
+    bank.addCustomerTransaction("Brasília", "Tim", 44.22);
+    bank.addCustomerTransaction("Brasília", "Tim", 12.44);
+    bank.addCustomerTransaction("Brasília", "Mike", 1.65);
+
+    bank.listCustomers("Brasília", true);
   }
 }
