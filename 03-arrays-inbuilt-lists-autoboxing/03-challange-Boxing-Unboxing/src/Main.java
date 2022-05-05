9544a593b8d69ac514b150ddf50f1050ce434835
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-05 07:43:29 -0300

    add new tests

--- 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Main.java
+++ 03-arrays-inbuilt-lists-autoboxing/03-challange-Boxing-Unboxing/src/Main.java
@@ -3,15 +3,19 @@ public class Main {
     Bank bank = new Bank("Banco Nacional");
 
     bank.addBranch("Brasília");
+    bank.addBranch("São Paulo");
 
     bank.addCustomer("Brasília", "Tim", 50.05);
     bank.addCustomer("Brasília", "Mike", 175.34);
     bank.addCustomer("Brasília", "Percy", 220.12);
+
+    bank.addCustomer("São Paulo", "Pedro", 54.06);
 
     bank.addCustomerTransaction("Brasília", "Tim", 44.22);
     bank.addCustomerTransaction("Brasília", "Tim", 12.44);
     bank.addCustomerTransaction("Brasília", "Mike", 1.65);
 
     bank.listCustomers("Brasília", true);
+    bank.listCustomers("São Paulo", true);
   }
 }
