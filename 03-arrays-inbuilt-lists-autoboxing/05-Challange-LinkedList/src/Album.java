f4e5b64f2faf88e4cadf5d19d4030d7d2030de5a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-06 13:49:38 -0300

    implements addSong and findSong methods

---
+++ 03-arrays-inbuilt-lists-autoboxing/05-Challange-LinkedList/src/Album.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Album {
+  private String name;
+  private String artist;
+  private ArrayList<Song> songs;
+
+  public Album(String name, String artist) {
+    this.name = name;
+    this.artist = artist;
+    this.songs = new ArrayList<Song>();
+  }
+
+  public boolean addSong(String title, double duration) {
+    // Preciso verificar se a música já existe
+    // Se não existir, adicionar no album e retornar true
+    // Se existir, não adicionar e retornar false
+    Song foundSong = findSong(title);
+
+    if (foundSong != null) {
+      System.out.println(title + " já existe no album");
+      return false;
+    }
+
+    Song newSong = new Song(title, duration);
+    this.songs.add(newSong);
+    return true;
+  }
+
+  public Song findSong(String title) {
+    for (Song song : this.songs) {
+      if (song.getTitle().equals(title)) {
+        return song;
+      }
+    }
+
+    return null;
+  }
+}
