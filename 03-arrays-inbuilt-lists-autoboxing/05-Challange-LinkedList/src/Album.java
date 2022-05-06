a7288ccad47812c62d81d92de659d30ede650573
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-06 15:02:28 -0300

    implements addToPlayList methods

--- 03-arrays-inbuilt-lists-autoboxing/05-Challange-LinkedList/src/Album.java
+++ 03-arrays-inbuilt-lists-autoboxing/05-Challange-LinkedList/src/Album.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
   private String name;
@@ -35,5 +36,40 @@ public class Album {
     }
 
     return null;
+  }
+
+  public boolean addToPlayList(int trackSongNumber, LinkedList<Song> songs) {
+    int albumSize = this.songs.size();
+
+    if (trackSongNumber > albumSize) {
+      System.out.println("Não foi possível adicionar a música na sua playlist." +
+          "\nA música com o número " + trackSongNumber + " não existe e/ou não foi adicionada em um álbum.");
+      return false;
+    }
+
+    Song foundSong = this.songs.get(trackSongNumber - 1);
+
+    if (foundSong != null) {
+      songs.add(foundSong);
+      return true;
+    }
+
+    return false;
+  }
+
+  public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+    // Preciso verificar se a música passada, já existe em algum álbum
+    // se tiver, eu adiciono ela no meu album e retorno true
+    // caso contrário, mando uma mensagem para o usuário e retorno false
+    Song foundSong = findSong(title);
+
+    if (foundSong != null) {
+      playlist.add(foundSong);
+      return true;
+    }
+
+    System.out.println("Não foi possível adicionar a música na sua playlist." +
+        "\nA música '" + title + "' não existe e/ou não foi adicionada em um álbum.");
+    return false;
   }
 }
