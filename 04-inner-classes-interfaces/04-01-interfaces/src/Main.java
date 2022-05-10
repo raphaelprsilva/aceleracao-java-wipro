5453306edb96b3ac3d3b5c061d3c0acee2dbf552
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-10 07:43:01 -0300

    add Main class

--- 04-inner-classes-interfaces/04-01-interfaces/src/Main.java
+++ 04-inner-classes-interfaces/04-01-interfaces/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
   public static void main(String[] args) {
+    ITelephone raphaPhone;
+    raphaPhone = new DeskPhone(123456);
+    raphaPhone.powerOn();
+    raphaPhone.callPhone(123456);
 
+    raphaPhone = new MobilePhone(456);
+    raphaPhone.powerOn();
+    raphaPhone.callPhone(456);
+    raphaPhone.answer();
   }
 }
