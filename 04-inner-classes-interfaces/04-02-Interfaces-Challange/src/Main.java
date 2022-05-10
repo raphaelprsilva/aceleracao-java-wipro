7b1b670451d27a974507286dc9c6b2797bcdbc45
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-10 13:38:41 -0300

    implements Main class

--- 04-inner-classes-interfaces/04-02-Interfaces-Challange/src/Main.java
+++ 04-inner-classes-interfaces/04-02-Interfaces-Challange/src/Main.java
@@ -1,5 +1,61 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Player player1 = new Player("rapha", 10, 50);
+    System.out.println(player1.toString());
+    saveObject(player1);
 
+    player1.setHitPoints(8);
+    System.out.println(player1);
+    player1.setWeapon("Stormbringer");
+    saveObject(player1);
+    // loadObject(player1);
+    System.out.println(player1);
+
+    Monster werewolf = new Monster("Werewolf", 20, 40);
+    System.out.println(werewolf.toString());
+    saveObject(werewolf);
+  }
+
+  public static ArrayList<String> readValues() {
+    ArrayList<String> values = new ArrayList<String>();
+
+    Scanner scanner = new Scanner(System.in);
+    boolean quit = false;
+    int index = 0;
+    System.out.println("Choose\n" +
+        "1 to enter a string\n" +
+        "0 to quit");
+
+    while (!quit) {
+      System.out.print("Choose an option: ");
+      int choice = scanner.nextInt();
+      scanner.nextLine();
+      switch (choice) {
+        case 0:
+          quit = true;
+          break;
+        case 1:
+          System.out.print("Enter a string: ");
+          String stringInput = scanner.nextLine();
+          values.add(index, stringInput);
+          index++;
+          break;
+      }
+    }
+    return values;
+  }
+
+  public static void saveObject(ISavable objectToSave) {
+    for (int index = 0; index < objectToSave.write().size(); index += 1) {
+      System.out.println("Saving " + objectToSave.write().get(index) + " to storage device.");
+    }
+  }
+
+  public static void loadObject(ISavable objectToLoad) {
+    ArrayList<String> values = readValues();
+    objectToLoad.read(values);
   }
 }
