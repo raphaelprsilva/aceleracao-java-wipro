3d2d73176fee4c3258750983f88ae29450ddf086
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-16 08:26:20 -0300

    update Main class

--- 04-inner-classes-interfaces/04-05-generics/src/Main.java
+++ 04-inner-classes-interfaces/04-05-generics/src/Main.java
@@ -2,6 +2,21 @@ import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
+    FootballPlayer romario = new FootballPlayer("Romario");
+    VoleyballPlayer giba = new VoleyballPlayer("Giba");
+    BaseballPlayer pat = new BaseballPlayer("Pat");
+
+    Team<FootballPlayer> flamengo = new Team<>("Flamengo");
+    flamengo.addPlayer(romario);
+    flamengo.getNumberOfPlayers();
+
+    Team<BaseballPlayer> aligators = new Team<>("Aligators");
+    aligators.addPlayer(pat);
+
+    Team<VoleyballPlayer> minas = new Team<>("Minas");
+    minas.addPlayer(giba);
+
+    // flamengo.matchResult(minas, 3, 1);
 
   }
 }
