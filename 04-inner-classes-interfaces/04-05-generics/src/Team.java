47f99b1f15c6f88e47e6f8d5dbefbf1d4e05704a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-17 07:10:24 -0300

    add compareTo method

--- 04-inner-classes-interfaces/04-05-generics/src/Team.java
+++ 04-inner-classes-interfaces/04-05-generics/src/Team.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>> {
   private final String name;
   int played = 0;
   int won = 0;
@@ -55,5 +55,15 @@ public class Team<T extends Player> {
 
   public int ranking() {
     return (this.won * 2) + tied;
+  }
+
+  @Override
+  public int compareTo(Team<T> team) {
+    if (this.ranking() > team.ranking()) {
+      return -1;
+    } else if (this.ranking() < team.ranking()) {
+      return 1;
+    }
+    return 0;
   }
 }
