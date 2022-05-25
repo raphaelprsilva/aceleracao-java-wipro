0b49ece4f23a0bbe5d63a4d4315b9cf5e77607da
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-25 09:08:55 -0300

    add ForEachLoop class

---
+++ 05-collections/05-02-collections/src/com/raphael/ForEachLoop.java
@@ -0,0 +1,12 @@
+package com.raphael;
+
+public class ForEachLoop {
+  public static void main(String[] args) {
+
+    double[] grades = { 9.3, 2.3, 8.4, 6.5 };
+
+    for (double grade : grades) {
+      System.out.println(grade);
+    }
+  }
+}
