79b880018c4ee0878f10997e237dea17a96350f4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-21 09:41:38 -0300

    remove files

--- 02-object-orientation/Polimorfism/src/ForgetableMovie.java
+++
@@ -1,7 +0,0 @@
-public class ForgetableMovie extends Movie {
-  public ForgetableMovie() {
-    super("Forgetable");
-  }
-
-  // No plot method
-}
--- 02-object-orientation/encapsulamento-e-heranca/src/Caminhao.java
+++
@@ -1,8 +0,0 @@
-public class Caminhao extends Veiculo{
-  private String placa;
-  private String motorista;
-
-  public Caminhao(String placa, String motorista, String cor) {
-    super(placa, motorista, cor);
-  }
-}
