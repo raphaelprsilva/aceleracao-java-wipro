841a05ca1e99ebbd526da2b2d5aeabc738e847ab
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-11 08:52:07 -0300

    add `constructors with parameters` files

--- 11-heranca/src/Main.java
+++ 11-heranca/src/Main.java
@@ -4,12 +4,10 @@ import br.com.elraphael.banco.Titular;
 
 public class Main {
   public static void main(String[] args) {
-    ContaEspecial conta1 = new ContaEspecial();
     Titular raphael = new Titular("Raphael", "11122233344");
-    conta1.setTitular(raphael);
-    conta1.setAgencia(1234);
-    conta1.setNumero(3322);
-    conta1.setLimiteChequeEspecial(1000);
+    Conta conta1 = new Conta(raphael, 555, 123);
+
+    // conta1.setLimiteChequeEspecial(1000);
 
     conta1.depositar(100);
     conta1.imprimirDemonstrativo();
--- 11-heranca/src/br/com/elraphael/banco/Conta.java
+++ 11-heranca/src/br/com/elraphael/banco/Conta.java
@@ -7,40 +7,27 @@ public class Conta {
   private int numero;
   private double saldo;
 
-  public Conta(double saldoInicial) {
-    this.saldo = saldoInicial;
+  public Conta(Titular titular, int agencia, int numero) {
+    this.titular = titular;
+    this.agencia = agencia;
+    this.numero = numero;
+    this.saldo = 0;
   }
 
   public Titular getTitular() {
     return titular;
   }
 
-  public void setTitular(Titular titular) {
-    this.titular = titular;
-  }
-
   public int getAgencia() {
     return agencia;
-  }
-
-  public void setAgencia(int agencia) {
-    this.agencia = agencia;
   }
 
   public int getNumero() {
     return numero;
   }
 
-  public void setNumero(int numero) {
-    this.numero = numero;
-  }
-
   public double getSaldo() {
     return saldo;
-  }
-
-  protected void setSaldo(double saldo) {
-    this.saldo = saldo;
   }
 
   protected void validarSaldoParaSaque(double valorSaque) {
