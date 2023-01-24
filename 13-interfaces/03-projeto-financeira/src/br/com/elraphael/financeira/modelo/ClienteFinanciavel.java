02fcf3fc44dfcbd3c49efbc00093a76945db2d1a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-24 20:26:56 -0300

    add `private methdos at interfaces` files

--- 13-interfaces/03-projeto-financeira/src/br/com/elraphael/financeira/modelo/ClienteFinanciavel.java
+++ 13-interfaces/03-projeto-financeira/src/br/com/elraphael/financeira/modelo/ClienteFinanciavel.java
@@ -3,11 +3,15 @@ package br.com.elraphael.financeira.modelo;
 public interface ClienteFinanciavel {
   double calcularLimiteAprovado();
   default double calcularJuros(double valorSolicitado) {
-    if (valorSolicitado <= 100_000) {
+    if (isFinanciamentoValido(valorSolicitado, 100_000)) {
       return 1.0;
-    } else if (valorSolicitado <= 1_000_000) {
+    } else if (isFinanciamentoValido(valorSolicitado, 1_000_000)) {
       return 1.5;
     }
     return 2.0;
-  };
+  }
+
+  private boolean isFinanciamentoValido(double valorSolicitado, int limiteBanco) {
+    return valorSolicitado <= limiteBanco;
+  }
 }
